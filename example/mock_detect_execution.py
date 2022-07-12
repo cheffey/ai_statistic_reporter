a908dd03b140ae1291c68738a9aed25c8085d74e
cheffey <feix@mtu.edu>
2022-07-12 10:47:13 +0800

    support multi class detection mAP calculation

--- example/mock_detect_execution.py
+++ example/mock_detect_execution.py
@@ -4,7 +4,7 @@ from reporter.util.file_util import Path
 from reporter.util.json_util import load_obj
 
 
-# [[82, 31, 185, 247], [249, 0, 321, 148], [334, 0, 383, 94], [414, 0, 453, 54]]
+# [{"rect": [82, 31, 185, 247], "obj_class": "human"}, {"rect": [249, 0, 321, 148], "obj_class": "dog"}]
 def to_exp_box(exp):
     x = exp.rect[0]
     y = exp.rect[1]
@@ -14,7 +14,7 @@ def to_exp_box(exp):
     return Box(x, y, w, h, clazz)
 
 
-# {'left_top': [82, 29], 'right_bottom': [188, 250]}, 'obj_class': 'face', 'obj_score': 0.9594442248344421}
+# {'left_top': [82, 29], 'right_bottom': [188, 250]}, 'obj_class': 'dog', 'obj_score': 0.9594442248344421}
 def to_res_box(res) -> ResBox:
     x = res.obj_bbox.left_top[0]
     y = res.obj_bbox.left_top[1]
